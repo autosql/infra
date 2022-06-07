febd1f4f37cdfebf1f537e4ba2a74d18f16640dd
heechankim <heechan.dev@gmail.com>
2022-06-07 18:53:06 +0000

    Feat: terraform auto script mvp

--- do-terraform.sh
+++ do-terraform.sh
@@ -2,21 +2,22 @@
 
 # Program settings
 ### COLOR
-RED='\033[0;91m'
-RED_B='\033[0;101m'
-GREEN='\033[0;32m'
-EOC='\033[0m'
+ERROR='\033[0;91m' # Red
+ERROR_B='\033[0;101m' # Background Red
+MSG='\033[0;32m' # Green
+INFO='\033[0;36m' # Cyan
+EOC='\033[0m' # End of Color
 
 
 # Check Input Arguments
 if [[ -z $1 || -z $2 || -z $3 ]]; then
-  echo -e "${RED}Use: $0 <PATH DIR> <TERRAFORM WORKSPACE> <TERRAFORM COMMAND>${EOC}"
-  echo -e "${GREEN}Example: $0 live/stage/vpc dev plan${EOC}"
+  echo -e "${ERROR}Use: $0 <PATH DIR> <TERRAFORM WORKSPACE> <TERRAFORM COMMAND>${EOC}"
+  echo -e "${MSG}Example: $0 live/stage/vpc dev plan${EOC}"
   exit -1
 fi
 
 
-# Check directory path
+# Check Directory path
 cd $1
 if [[ $? -ne 0 ]]; then
   exit -1
@@ -30,24 +31,33 @@ T_WORKSPACES=('dev' 'prod')
 if [[ " ${T_WORKSPACES[*]} " =~ " $2 " ]]; then
   WORKSPACE=$2
 else
-  echo -e "${RED}Workspace $2 is not available${EOC}"
-  echo -e "${GREEN}Available Workspaces are: ${T_WORKSPACES[@]}${EOC}"
+  echo -e "${ERROR}Workspace $2 is not available${EOC}"
+  echo -e "${MSG}Available Workspaces are: ${T_WORKSPACES[@]}${EOC}"
   exit -1
 fi
 
-# Check terraform command
+# Check Terraform command
 T_COMMANDS=('plan' 'apply' 'destroy')
 
 if [[ " ${T_COMMANDS[*]} " =~ " $3 " ]]; then
-  COMMAND="terraform $2"
+  COMMAND="terraform $3"
 else
-  echo -e "${RED}Command $3 is not available${EOC}"
-  echo -e "${GREEN}Available Commands are: ${T_COMMANDS[@]}${EOC}"
+  echo -e "${ERROR}Command $3 is not available${EOC}"
+  echo -e "${MSG}Available Commands are: ${T_COMMANDS[@]}${EOC}"
   exit -1
 fi
 
+# Check Terraform file exists
+T_FILES=`find . -type f -name "*.tf"`
+if [[ ${#T_FILES[@]} -le 0 ]]; then
+  echo "${ERROR}There is no terraform file${EOC}"
+  exit -1
+fi
+
+echo -e "\n"
+
 # Show pwd message
-echo -e "Current Directory: ${RED}`pwd`${EOC}"
+echo -e "${MSG}Current Directory: ${INFO}`pwd`${EOC}"
 
 # Change Terraform Workspace
 # If there is no workspace Then Create it
@@ -56,15 +66,15 @@ if [[ $? -ne 0 ]]; then
   echo -e "There is no workspace $2"
   terraform workspace new $2
 fi
-echo -e "Current Workspace: ${RED_B}`terraform workspace show`${EOC}"
+echo -e "${MSG}Current Workspace: ${ERROR_B}`terraform workspace show`${EOC}"
 
-
-T_FILES=`find . -type f -name "*.tf"`
-if [[ ${#T_FILES[@]} -le 0 ]]; then
-  echo "There is no terraform file"
-  exit -1
+# Make Command with var files
+T_VARS=`find . -type f -name "*.tfvars"`
+if [[ ${#T_VARS[@]} -gt 0 ]]; then
+  for vars in "${T_VARS[@]}"; do
+    COMMAND="$COMMAND -var-file=$vars"
+  done
 fi
 
-echo -e "Command will execute: $COMMAND"
-echo -e "Directory: $DIR"
+echo -e "${MSG}Command will execute: ${INFO}$COMMAND${EOC}"
 
