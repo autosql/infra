414572ff587f17e3b2d1a9fd146046601273fc77
heechankim <heechan.dev@gmail.com>
2022-06-07 18:19:33 +0000

    Add: Change dir and Change workspace

--- do-terraform.sh
+++ do-terraform.sh
@@ -8,13 +8,31 @@ GREEN='\033[0;32m'
 EOC='\033[0m'
 
 # Check Input Arguments
-if [[ -z $1 || -z $2 ]]; then
-  echo "Use: $0 <PATH DIR> <TERRAFORM COMMAND"
-  echo "Example: $0 live/stage/vpc plan"
+if [[ -z $1 || -z $2 || -z $3 ]]; then
+  echo -e "${RED}Use: $0 <PATH DIR> <TERRAFORM WORKSPACE> <TERRAFORM COMMAND>${EOC}"
+  echo -e "${GREEN}Example: $0 live/stage/vpc dev plan${EOC}"
   exit -1
 fi
 
+# Change Directory
+cd $DIR
+if [[ $? -ne 0 ]]; then
+  exit -1
+fi
+echo -e "Current Directory: ${RED}`pwd`${EOC}"
 
+# Change Terraform Workspace
+# If there is no workspace Then Create it
+terraform workspace select $2
+if [[ $? -ne 0 ]]; then
+  echo -e "There is no workspace $2"
+  terraform workspace new $2
+fi
+echo -e "Current Workspace: ${RED_B}`terraform workspace show`${EOC}"
+
+
+# Check terraform command
+T_COMMANDS=('plan' 'apply' 'destroy')
 
 
 # TERRAFORM COMMAND
@@ -26,20 +44,12 @@ DIR=$1
 # VAR FILES
 
 
-
-# Change Directory
-cd $DIR
-if [[ $? -ne 0 ]]; then
-  exit -1
-fi
-
 T_FILES=`find . -type f -name "*.tf"`
 if [[ ${#T_FILES[@]} -le 0 ]]; then
   echo "There is no terraform file"
   exit -1
 fi
 
-echo -e "Current Workspace: ${RED_B}`terraform workspace show`${EOC}"
 echo -e "Command will execute: $COMMAND"
 echo -e "Directory: $DIR"
 
