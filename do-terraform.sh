a1e416e2a3be0b02f9ceb3803bde75b9a9ca51d8
heechankim <heechan.dev@gmail.com>
2022-06-07 18:33:17 +0000

    Add: available commands and workspace restriction

--- do-terraform.sh
+++ do-terraform.sh
@@ -7,6 +7,7 @@ RED_B='\033[0;101m'
 GREEN='\033[0;32m'
 EOC='\033[0m'
 
+
 # Check Input Arguments
 if [[ -z $1 || -z $2 || -z $3 ]]; then
   echo -e "${RED}Use: $0 <PATH DIR> <TERRAFORM WORKSPACE> <TERRAFORM COMMAND>${EOC}"
@@ -14,11 +15,38 @@ if [[ -z $1 || -z $2 || -z $3 ]]; then
   exit -1
 fi
 
-# Change Directory
-cd $DIR
+
+# Check directory path
+cd $1
 if [[ $? -ne 0 ]]; then
   exit -1
 fi
+DIR=$1
+
+
+# Check Workspace restriction
+T_WORKSPACES=('dev' 'prod')
+
+if [[ " ${T_WORKSPACES[*]} " =~ " $2 " ]]; then
+  WORKSPACE=$2
+else
+  echo -e "${RED}Workspace $2 is not available${EOC}"
+  echo -e "${GREEN}Available Workspaces are: ${T_WORKSPACES[@]}${EOC}"
+  exit -1
+fi
+
+# Check terraform command
+T_COMMANDS=('plan' 'apply' 'destroy')
+
+if [[ " ${T_COMMANDS[*]} " =~ " $3 " ]]; then
+  COMMAND="terraform $2"
+else
+  echo -e "${RED}Command $3 is not available${EOC}"
+  echo -e "${GREEN}Available Commands are: ${T_COMMANDS[@]}${EOC}"
+  exit -1
+fi
+
+# Show pwd message
 echo -e "Current Directory: ${RED}`pwd`${EOC}"
 
 # Change Terraform Workspace
@@ -29,19 +57,6 @@ if [[ $? -ne 0 ]]; then
   terraform workspace new $2
 fi
 echo -e "Current Workspace: ${RED_B}`terraform workspace show`${EOC}"
-
-
-# Check terraform command
-T_COMMANDS=('plan' 'apply' 'destroy')
-
-
-# TERRAFORM COMMAND
-COMMAND="terraform $2"
-
-# WORKING DIRECTORY
-DIR=$1
-
-# VAR FILES
 
 
 T_FILES=`find . -type f -name "*.tf"`
