c4289755ea175044a92b39cfafe77ba11b7b3cc5
heechankim <heechan.dev@gmail.com>
2022-06-08 12:24:47 +0000

    Change: frontend output

--- dev/frontend/outputs.tf
+++ dev/frontend/outputs.tf
@@ -1,19 +1,25 @@
+# ----- s3 bucket
+
 output "bucket_domain_name" {
-  value = module.frontend.bucket_domain_name
+  value = module.frontend.bucket_domain_name 
 }
 
 output "bucket_regional_domain_name" {
-  value = module.frontend.bucket_regional_domain_name
+  value = module.frontend.bucket_regional_domain_name 
 }
 
+## ----- S3 bucket website configuration
+/*
 output "website_domain" {
-  value = module.frontend.website_domain
+  value = module.frontend.website_domain 
 }
 
 output "website_endpoint" {
-  value = module.frontend.website_endpoint
+  value = module.frontend.website_endpoint 
 }
+*/
+# ----- cloudfront
 
 output "domain_name" {
-  value = module.frontend.domain_name
+  value = module.frontend.domain_name 
 }
