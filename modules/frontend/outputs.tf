e8e18ee13f8e384e32a15e255c77093b0b6b21cb
heechankim <heechan.dev@gmail.com>
2022-06-09 17:01:01 +0000

    Create: cloudfront - s3 dns record and acm

--- modules/frontend/outputs.tf
+++ modules/frontend/outputs.tf
@@ -24,3 +24,7 @@ output "website_endpoint" {
 output "domain_name" {
   value = aws_cloudfront_distribution.this.domain_name
 }
+
+output "hosted_zone_id" {
+  value = aws_cloudfront_distribution.this.hosted_zone_id
+}
