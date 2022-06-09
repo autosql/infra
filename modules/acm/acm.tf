2635f3576001bb1631eb489deeecb464b2fd6d82
heechankim <heechan.dev@gmail.com>
2022-06-09 17:22:18 +0000

    Create: acm module

--- modules/acm/acm.tf
+++ modules/acm/acm.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "this" {
-  domain_name = "*.${var.env}.${var.domain}"
+  domain_name = "*.${var.domain}"
   validation_method = "DNS"
 
   tags = {
---
+++ modules/acm/variables.tf
@@ -0,0 +1,15 @@
+variable "app" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
+
+variable "route53_zone_id" {
+  type = string
+}
