a7761f2e6d447d7213dad74678f2d53864376ed2
heechankim <heechan.dev@gmail.com>
2022-06-11 01:51:11 +0000

    Add: cache policy

--- modules/cloudfront/cloudfront.tf
+++ modules/cloudfront/cloudfront.tf
@@ -3,6 +3,28 @@ variable "default_bucket" {
   default = "landing"
 }
 
+resource "aws_cloudfront_cache_policy" "this" {
+  name = "CachingOptimized"
+  comment = "Default policy when CF compression is enabled"
+  default_ttl = 50
+  max_ttl = 100
+  min_ttl = 1
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    enable_accept_encoding_gzip = true
+    enable_accept_encoding_brotli = true
+    headers_config {
+      header_behavior = "none"
+    }
+    cookies_config {
+      cookie_behavior = "none"
+    } 
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
+}
+
 resource "aws_cloudfront_distribution" "this" {
   enabled = true
   aliases = ["${local.domain_name}"]
@@ -13,7 +35,6 @@ resource "aws_cloudfront_distribution" "this" {
     content {
       domain_name = origin.value
       origin_id = origin.key
-      origin_path = "%{if origin.key != var.default_bucket}/${origin.key}/*%{endif}"
 
       custom_origin_config {
         http_port              = 80
@@ -29,13 +50,7 @@ resource "aws_cloudfront_distribution" "this" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = var.default_bucket
 
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
+    cache_policy_id = aws_cloudfront_cache_policy.this.id
 
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
@@ -51,7 +66,30 @@ resource "aws_cloudfront_distribution" "this" {
     }
 
     content {
-      path_pattern = "/${ordered_cache_behavior.key}/*"
+      path_pattern = "/${ordered_cache_behavior.key}" 
+
+      allowed_methods  = ["GET", "HEAD"]
+      cached_methods   = ["GET", "HEAD"]
+      target_origin_id = ordered_cache_behavior.key
+
+      cache_policy_id = aws_cloudfront_cache_policy.this.id
+
+      viewer_protocol_policy = "allow-all"
+      min_ttl                = 0
+      default_ttl            = 3600
+      max_ttl                = 86400
+    }
+  }
+
+  dynamic "ordered_cache_behavior" {
+    for_each = {
+      for k, val in var.bucket_domain_names :
+      k => val
+      if k != var.default_bucket
+    }
+
+    content {
+      path_pattern = "index.html" 
 
       allowed_methods  = ["GET", "HEAD"]
       cached_methods   = ["GET", "HEAD"]
@@ -67,8 +105,8 @@ resource "aws_cloudfront_distribution" "this" {
 
       viewer_protocol_policy = "allow-all"
       min_ttl                = 0
-      default_ttl            = 3600
-      max_ttl                = 86400
+      default_ttl            = 0
+      max_ttl                = 0
     }
   }
 
