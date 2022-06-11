01a2b2de79bafc56e25ce532912cb96fbfbc6047
heechankim <heechan.dev@gmail.com>
2022-06-11 19:23:25 +0000

    Add: iam role for ecs task in backend module

--- modules/backend/elastic-container-service.tf
+++ modules/backend/elastic-container-service.tf
@@ -1,5 +1,5 @@
 # ----- vpc security group
-# depend_on aws_security_group.lb
+## depend_on aws_security_group.lb
 resource "aws_security_group" "ecs" {
   vpc_id = var.vpc_id
   name = "${local.prefix}-ecs"
@@ -19,4 +19,46 @@ resource "aws_security_group" "ecs" {
     to_port = 0
     cidr_blocks = local.all_ips
   }
+
+  tags = merge(
+    local.tags, {
+      Name = "${local.prefix}-ecs-tasks"
+    }
+  )
 }
+
+# ----- ecs cluster
+resource "aws_ecs_cluster" "this" {
+  name = "${local.prefix}-cluster"
+  tags = local.tags
+}
+
+## ----- iam roles
+## iam role policy document
+data "aws_iam_policy_document" "ecs_task" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+## iam role
+resource "aws_iam_role" "ecs_task" {
+  name = "${local.prefix}-ecs-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task.json
+
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task" {
+  role = aws_iam_role.ecs_task.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+## ----- task definition
+
