78fab50fdd2f27727c3ebe9799dc68a912479707
heechankim <heechan.dev@gmail.com>
2022-06-11 18:30:23 +0000

    Chore: vpc output and variables

--- dev/vpc/main.tf
+++ dev/vpc/main.tf
@@ -1,15 +1,10 @@
+provider "aws" {
+  region = var.region
+}
+
 module "vpc" {
   source = "../../modules/vpc"
 
   app = var.app
   env = terraform.workspace
 }
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
-
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,3 +5,7 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public.*.id
 }
+
+output "igw_id" {
+  value = aws_internet_gateway.this.id
+}
