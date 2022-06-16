186d8608add162f4197a6cbd354b958ab673c13e
heechankim <heechan.dev@gmail.com>
2022-06-16 23:03:48 +0000

    Chore: remove acm and nginx config file

--- modules/frontend/route53.tf
+++ modules/frontend/route53.tf
@@ -13,31 +13,3 @@ resource "aws_route53_record" "main" {
   records = [aws_eip.proxy.public_ip]
 
 }
-
-# ----- ACM Certificate
-resource "aws_acm_certificate" "this" {
-  domain_name = "${local.domain_name}"
-  validation_method = "DNS"
-}
-
-resource "aws_route53_record" "this" {
-  for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.this.id
-}
-
-resource "aws_acm_certificate_validation" "dns_validation" {
-  certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
-}
